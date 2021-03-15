1644772666b93730ac88dcbd132b9f8db248823f
EtWn <34377743+EtWnn@users.noreply.github.com>
2021-03-15 14:56:35 +0100

    Re table (#4)
    * upgrade Table class with setaatr for cols and key attributes
    
    * apdate database to the new Table class and update the tests
    
    * reformat columns name for SQL convs
    
    * reformat BinanceDataBase and BinanceManager for the new table format

--- src/storage/tables.py
+++ src/storage/tables.py
@@ -1,31 +1,46 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 
-@dataclass
 class Table:
-    name: str
-    columns_names: List[str]
-    columns_sql_types: List[str]
+    """
+    @DynamicAttrs
+    """
+
+    def __init__(self, name: str, columns_names: List[str], columns_sql_types: List[str],
+                 primary_key: Optional[str] = None, primary_key_sql_type: Optional[str] = None):
+        self.name = name
+        self.columns_names = columns_names
+        self.columns_sql_types = columns_sql_types
+        self.primary_key = primary_key
+        self.primary_key_sql_type = primary_key_sql_type
+
+        for column_name in self.columns_names:
+            try:
+                value = getattr(self, column_name)
+                raise ValueError(f"the name {column_name} conflicts with an existing attribute of value {value}")
+            except AttributeError:
+                setattr(self, column_name, column_name)
+
+        if self.primary_key is not None:
+            setattr(self, self.primary_key, self.primary_key)
 
 
 SPOT_TRADE_TABLE = Table(
     'spot_trade',
     [
-        'key',
-        'id',
-        'millistamp',
+        'tradeId',
+        'tdTime',
         'asset',
-        'ref_asset',
+        'refAsset',
         'qty',
         'price',
         'fee',
-        'fee_asset',
+        'feeAsset',
         'isBuyer'
 
     ],
     [
-        'TEXT',
         'INTEGER',
         'INTEGER',
         'TEXT',
@@ -41,24 +56,23 @@ SPOT_TRADE_TABLE = Table(
 SPOT_DEPOSIT_TABLE = Table(
     'spot_deposit',
     [
-        'txId',
         'insertTime',
         'asset',
         'amount',
     ],
     [
-        'TEXT',
         'INTEGER',
         'TEXT',
         'REAL'
-    ]
+    ],
+    primary_key='txId',
+    primary_key_sql_type='TEXT'
 )
 
 
 SPOT_WITHDRAW_TABLE = Table(
     'spot_withdraw',
     [
-        'id',
         'txId',
         'applyTime',
         'asset',
@@ -67,42 +81,43 @@ SPOT_WITHDRAW_TABLE = Table(
     ],
     [
         'TEXT',
-        'TEXT',
         'INTEGER',
         'TEXT',
         'REAL',
         'REAL'
-    ]
+    ],
+    primary_key='withdrawId',
+    primary_key_sql_type='TEXT'
 )
 
 SPOT_DIVIDEND_TABLE = Table(
     'spot_dividend_table',
     [
-        'id',
         'divTime',
         'asset',
         'amount'
     ],
     [
         'INTEGER',
-        'INTEGER',
         'TEXT',
         'REAL'
-    ]
+    ],
+    primary_key='divId',
+    primary_key_sql_type='INTEGER'
 )
 
 SPOT_DUST_TABLE = Table(
     'spot_dust_table',
     [
-        'id',
-        'time',
+        'tranId',
+        'dustTime',
         'asset',
-        'asset_amount',
-        'bnb_amount',
-        'bnb_fee',
+        'assetAmount',
+        'bnbAmount',
+        'bnbFee',
     ],
     [
-        'TEXT',
+        'INTEGER',
         'INTEGER',
         'TEXT',
         'REAL',
@@ -114,14 +129,12 @@ SPOT_DUST_TABLE = Table(
 LENDING_INTEREST_TABLE = Table(
     'lending_interest_table',
     [
-        'id',
-        'time',
-        'lending_type',
+        'interestTime',
+        'lendingType',
         'asset',
         'amount',
     ],
     [
-        'TEXT',
         'INTEGER',
         'TEXT',
         'TEXT',
