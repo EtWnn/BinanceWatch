77ff6462b5ff188f94dc544df9cfd55ef8022c74
EtWnn <eti1ww@gmail.com>
2021-03-29 16:27:20 +0200

    auto version name for conf.py and setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+import os
 from setuptools import setup
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+about = {}
+with open(os.path.join(this_directory, 'BinanceWatch/__init__.py'), encoding='utf-8') as f:
+    exec(f.read(), about)
 
 setup(
     name='BinanceWatch',
-    version='0.1.1',
+    version=about['__version__'],
     packages=['BinanceWatch',
               'tests',
               'BinanceWatch.storage',
